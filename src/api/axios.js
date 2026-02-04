38767c6813891c1c1397b81add7f66ffdd43be3d
Abdellah EL GHENNAMI <abdellahelghanami67@gmail.com>
2026-02-04 13:33:17 +0100

    edit backend domain

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "http://localhost:3000/api/v1",
+    baseURL: "nestquest-backend-production.up.railway.app/api/v1",
     withCredentials: true
 })
