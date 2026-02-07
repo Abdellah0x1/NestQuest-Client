5c60f51cca9f9dbd73ce9ff23eb891e183619f7f
Abdellah EL GHENNAMI <abdellahelghanami67@gmail.com>
2026-02-07 11:12:01 +0100

    .

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "http://localhost:3000/api/v1",
+    baseURL: "https://nestquest-backend-production.up.railway.app/api/v1",
     withCredentials: true,
     headers: {
     "Content-Type": "application/json",
   },
 })
 
-// baseURL: "https://nestquest-backend-production.up.railway.app/api/v1",
+// baseURL: "",
